07de80417ded03448183604ea5e62235507c5812
root <root@gsot@corp.ru>
2019-02-04 16:41:55 +0300

    Sverki_Transport

--- sverki_rep.sh
+++ sverki_rep.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-#curdate="`date \+\%Y_\%m_\%d`"
-#dir="/var/www/files/Sverki/Reports"
+dir="/var/www/files/Sverki/Reports"
+echo $curdate
+num=`cat $(find $dir -maxdepth 1 -type f  -mtime -1) | wc -l`
+echo "NUM=$num"
+name=`find $dir -maxdepth 1 -type f  -mtime -1`
 
-#num=$(ls -l $dir | awk '{print$9}' | tail -n 1 | wc -l)
-#name=$(ls -l $dir | awk '{print$9}' | tail -n 1)
-#if [[ $num -ge 1 ]]; then
-#       echo "См. вложения" | mail -s "Отчеты по внесённым суммам" -a $dir/$name -r "gsot@corp.ws" gsverki@dmcorp.ru
-#       echo "См. вложения" | mail -s "Отчеты по внесённым суммам" -a $dir/$name -r "gsot@corp.ws" it@int.dmcorp.ru
-#fi
-#       rm $dir/$name
+echo "NAME=$name"
 
+if [ $num -ge 1 ]; then
+        echo "См. вложения" | mail -s "Отчеты по внесённым суммам" -a $name -r "gsot@corp.ws" gsverki@dmcorp.ru
+        echo "См. вложения" | mail -s "Отчеты по внесённым суммам" -a $name -r "gsot@corp.ws" it@int.dmcorp.ru
+else
+       rm  $name
+       echo "file is empty"
+fi
 
 ssh -T 10.10.1.4 << EOF                
 file1="/media/landisc/corp/TARIF/transport.xls"
